67240c656a8d61edcba6b042ec801da73b8fb878
rashmoni <rashudey9851@gmail.com>
2024-03-13 13:33:35 +0100

    first commit

---
+++ src/main/java/com/example/jibacrexample/JibAcrExampleApplication.java
@@ -0,0 +1,21 @@
+package com.example.jibacrexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JibAcrExampleApplication {
+
+	@GetMapping("/mesasage")
+	public String message(){
+		return "Welcome to Jib-acr-example application";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibAcrExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jibacrexample/JibAcrExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.jibacrexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibAcrExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("This test run");
+	}
+
+}
